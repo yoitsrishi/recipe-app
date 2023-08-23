43d3a00a74b1f3828262395bdcbc9fee55ae16a2
yoitsrishi <mrcodinggeek@gmail.com>
2023-08-23 20:48:31 +0530

    Added navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipees';
+  featureSelected:string = "";
+  getNavigation(feature:string){
+    this.featureSelected = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+
+  getFeature(feature:string){
+    this.featureSelected.emit(feature);
+
+  }
 }
